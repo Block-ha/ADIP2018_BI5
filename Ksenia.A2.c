51fc7b112924ca7731303ca1a98eec86c60d02fa
Block-ha <43049240+Block-ha@users.noreply.github.com>
2018-10-24 21:57:24 +0200

    Update and rename Ksenia.A2.1.c to Ksenia.A2.c

---
+++ Ksenia.A2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <studib.h>
+
+int main (){
+  unsigned int n;
+  unsigned int i = 1;
+  unsigned int summe = 0;
+  unsigned int q = 1;
+  unsigned int qsumme = 0;
+  
+  printf ("Geben Sie eine positive Ganzzahl 'n' ein......");
+  scanf ("%i", &n);
+  
+  while (i <= n){
+    q=i*i;
+    qsumme=qsumme+q;
+    summe=summe+i;
+    i++;
+    }
+
+  printf ("Die Summe der Zahlen von 1 bis n: %i,\nDie Summe der Quadratzahlen von 1 bis n: %i", summe, qsumme); 
+  return 0;
+}
